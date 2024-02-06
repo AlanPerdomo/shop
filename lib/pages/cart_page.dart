10c0d6f8e6b889675fb0f44f8d8d8e40532061f2
Alan Perdomo <alanperdomo@hotmail.com>
2024-02-06 15:56:09 -0300

    #?

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/cart_item.dart';
+import 'package:shop/models/order_list.dart';
 import '../models/cart.dart';
 
 class CartPage extends StatelessWidget {
@@ -45,7 +46,13 @@ class CartPage extends StatelessWidget {
                   ),
                   const Spacer(),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<OrderList>(
+                        context,
+                        listen: false,
+                      ).addOrder(cart);
+                      cart.clear();
+                    },
                     style: TextButton.styleFrom(
                       foregroundColor:
                           Theme.of(context).textTheme.bodyMedium?.color,
