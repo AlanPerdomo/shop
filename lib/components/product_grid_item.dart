a2dda2f588228c0448e8c50f9f6253bdb208089e
Alan Perdomo <alanperdomo@hotmail.com>
2024-05-22 10:59:24 -0300

    #323

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -63,9 +63,15 @@ class ProductGridItem extends StatelessWidget {
               arguments: product,
             );
           },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            transitionOnUserGestures: true,
+            child: FadeInImage(
+              placeholder:
+                  const AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
